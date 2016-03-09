5d8dcd73bc6dd87c8d77e9c311f3062365741a48
MikaelPhilip <philip.mikael@gmail.com>
2016-03-09 14:13:49 +0100

    Base projet

---
+++ Sir_tp7_nosql/src/main/java/Sir_tp7_nosql/Sir_tp7_nosql/App.java
@@ -0,0 +1,13 @@
+package Sir_tp7_nosql.Sir_tp7_nosql;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
